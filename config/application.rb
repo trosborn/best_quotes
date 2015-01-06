b03d5cd6bffe45d70505fc5ddbfcadc254ddc87f
Thomas Osborn <trosborn@gmail.com>
2015-01-06 21:07:59 +1200

    working through chapter 4

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require "thomler"
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
-require "quotes_controller"
-
 module BestQuotes
   class Application < Thomler::Application
   end
