e40035836d5629ba6e79adf1fd1e803b0bbebcfa
Thomas Osborn <trosborn@gmail.com>
2015-01-25 01:40:55 -0800

    working through chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,26 @@
 class QuotesController < Thomler::Controller
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
   def a_quote
     render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
   end
 
   def exception
